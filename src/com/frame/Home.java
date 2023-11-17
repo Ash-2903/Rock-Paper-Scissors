0d47d08d52530d687adad8c22f60a47a63496401
ashritha herle <ashrithaherle2903@gmail.com>
2023-11-17 20:08:35 +0530

    change ui

--- src/com/frame/Home.java
+++ src/com/frame/Home.java
@@ -47,9 +47,9 @@ public class Home extends JFrame implements ActionListener {
 		
 		// -------------- initial window --------------------------
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(500, 100, 500, 600);
+		setBounds(500, 100, 700, 600);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(201, 200, 255));
+		contentPane.setBackground(new Color(227, 227, 255));
 		contentPane.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
 
 		setContentPane(contentPane);
@@ -73,16 +73,16 @@ public class Home extends JFrame implements ActionListener {
 		
 		// 1) System score Label 
 		sysScoreLabel = new JLabel("Computer : 0");
-		sysScoreLabel.setBounds(0,222,476,30);
+		sysScoreLabel.setBounds(170,378,277,30);
 		sysScoreLabel.setFont(new Font("Bookman Old Style", Font.BOLD, 26)); 
 		sysScoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		getContentPane().add(sysScoreLabel);
 		
 		// 2) System Choice
-		sysChoice = new JLabel("?");
+		sysChoice = new JLabel("?"); 
 		sysChoice.setBackground(new Color(255, 255, 255));
 		sysChoice.setOpaque(true);
-		sysChoice.setBounds(185,94,98,81);
+		sysChoice.setBounds(89,141,98,81);
 		sysChoice.setFont(new Font("Dialog",Font.PLAIN,18));
 		sysChoice.setHorizontalAlignment(SwingConstants.CENTER);
 		sysChoice.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
@@ -90,7 +90,7 @@ public class Home extends JFrame implements ActionListener {
 		
 		// 3) Player score Label
 		playerScoreLabel = new JLabel("Player : 0");
-		playerScoreLabel.setBounds(0,291,476,30);
+		playerScoreLabel.setBounds(202,443,212,30);
 		playerScoreLabel.setFont(new Font("Bookman Old Style", Font.BOLD, 26)); 
 		playerScoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		getContentPane().add(playerScoreLabel);
@@ -99,22 +99,28 @@ public class Home extends JFrame implements ActionListener {
 		
 		// -> Rock
 		buttonRock = new JButton("Rock");
-		buttonRock.setBounds(60,387,105,81);
+		buttonRock.setBounds(451,37,105,81);
 		buttonRock.setFont(new Font("Dialog",Font.PLAIN,18));
 		buttonRock.addActionListener(this);
 		getContentPane().add(buttonRock);
 		// -> Paper
 		buttonPaper = new JButton("Paper");
-		buttonPaper.setBounds(185,387,105,81);
+		buttonPaper.setBounds(451,141,105,81);
 		buttonPaper.setFont(new Font("Dialog",Font.PLAIN,18));
 		buttonPaper.addActionListener(this);
 		getContentPane().add(buttonPaper);
 		// -> Scissor
 		buttonScissor = new JButton("Scissor");
-		buttonScissor.setBounds(310,387,105,81);
+		buttonScissor.setBounds(451,248,105,81);
 		buttonScissor.setFont(new Font("Dialog",Font.PLAIN,18));
 		buttonScissor.addActionListener(this);
 		getContentPane().add(buttonScissor);
+		
+		JLabel lblNewLabel = new JLabel("VS");
+		lblNewLabel.setForeground(new Color(128, 128, 128));
+		lblNewLabel.setFont(new Font("Wide Latin", Font.PLAIN, 49));
+		lblNewLabel.setBounds(249, 128, 138, 131);
+		contentPane.add(lblNewLabel);
 		
 		// showPopUp("test message");
 		
@@ -162,7 +168,4 @@ public class Home extends JFrame implements ActionListener {
 		showPopUp(result);
 		
 	}
-	
-	
-
 }
